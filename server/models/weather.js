d90ce4adbc42843977f38fb296d3f8120e64b3e9
silin-syngenta <86247852+silin-syngenta@users.noreply.github.com>
2021-09-11 18:12:50 -0400

    can get uvi and changed temp to Fg

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,14 +1,30 @@
 const axios = require("axios");
-
+const zipCodeGeoJson = require("../assets/zip-codes-to-geo-coords.json");
 const get = async (zipCode, cb) => {
-    const url = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},&appid=${process.env.API_KEY}`;
-    let response = await axios.get(url);
+    let [lat, lon] = zipCodeGeoJson[zipCode];
+    try {
+        let uvRes = await getUv(lon, lat);
+        let uvi = uvRes.current.uvi;
 
+        const url = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&units=imperial&appid=${process.env.API_KEY}`;
+        let response = await axios.get(url);
+        const picked = (({ list, city }) => ({ list, city }))(response.data);
+
+        picked.uvi = uvi;
+        cb(null, picked);
+    } catch (error) {
+        throw new Error("failed to get data", error);
+    }
+};
+
+const getUv = async (lon, lat) => {
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${process.env.API_KEY}`;
+    let response = await axios.get(url);
     if (response.error) {
         throw new Error("failed to get data", response.error);
     }
 
-    cb(null, response.data);
+    return response.data;
 };
 module.exports = {
     get,
